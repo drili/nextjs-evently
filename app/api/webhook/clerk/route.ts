2b6d0d45dc2915d1e76cba271cbc55873d6dd184
drili <DriliB@hotmail.com>
2023-12-25 22:28:08 +0100

    :bug:

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -2,7 +2,7 @@ import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
-import { clerkClient } from "@clerk/nextjs"
+import { clerkClient } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
@@ -54,8 +54,8 @@ export async function POST(req: Request) {
     const { id } = evt.data;
     const eventType = evt.type;
 
-    if (eventType === "user.created") {
-        const { id, email_addresses, image_url, first_name, last_name, username } = evt.data
+    if (eventType === 'user.created') {
+        const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
         const user = {
             clerkId: id,
@@ -66,7 +66,7 @@ export async function POST(req: Request) {
             photo: image_url,
         }
 
-        const newUser = await createUser(user)
+        const newUser = await createUser(user);
 
         if (newUser) {
             await clerkClient.users.updateUserMetadata(id, {
@@ -76,7 +76,7 @@ export async function POST(req: Request) {
             })
         }
 
-        return NextResponse.json({ message: "OK", user: newUser })
+        return NextResponse.json({ message: 'OK', user: newUser })
     }
 
     if (eventType === 'user.updated') {
