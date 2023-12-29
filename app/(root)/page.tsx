4e99d90c05346cbcfb0e4d2265b1126786705d17
drili <DriliB@hotmail.com>
2023-12-29 21:51:17 +0100

    ...

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -27,7 +27,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
                 <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
                     
                     <div className="flex flex-col justify-center gap-8">
-                        <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform</h1>
+                        <h1 className="h1-bold">Host now, Connect, Celebrate: Your Events, Our Platform</h1>
                         <p className="p-regular-20 md:p-regular-24">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque libero iusto perferendis quis, nobis ex. Numquam asperiores pariatur nesciunt nemo.</p>
 
                         <Button size="lg" asChild className="button w-full sm:w-fit">
